f546c1c8e94ab06719386bca86bc718d585575a8
Sobhan <sobhan.saf79@gmail.com>
2020-09-13 18:37:08 +0430

    website is scraped and data is saved in a database

--- scrap.py
+++ scrap.py
@@ -5,9 +5,8 @@ import material
 import price
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options  
-import datetime
+from datetime import datetime
 from time import sleep
-
 
 
 url = 'https://www.tgju.org/'
@@ -31,6 +30,8 @@ driver.close()
 soup = BeautifulSoup(html, 'html.parser')
 tables = soup.select(f'.{table_class_name}')
 
+db = database.DB()
+
 file = open('test', 'w')
 for table in tables:
     items = table.select('tbody tr')
@@ -38,5 +39,14 @@ for table in tables:
         name = item.find('th').text
         price = item.find('td').text
     
-        print(name, price, file=file)
+        price = price.replace('میلیون', '')  # some of prices has additional words
+        price = price.replace(',', '')
+        try:  # price should be numeric
+            price = int(price)
+        except:
+            continue
+
+        db.add_material(name)
+        utctime = int((datetime.now() - datetime(1970, 1, 1)).total_seconds())
+        db.add_price(name, utctime, price)
 

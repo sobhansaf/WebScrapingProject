5180bad9858f2d9a210985679c43502afc88bf72
Sobhan <sobhan.saf79@gmail.com>
2020-09-13 11:09:36 +0430

    created a class for materials and a class for prices

---
+++ material.py
@@ -0,0 +1,4 @@
+class material:
+    def __init__(self, id, name):
+        self.name = name
+        self.id = id
---
+++ price.py
@@ -0,0 +1,6 @@
+class price:
+    def __init__(self, id, material_id, price, date):
+        self.id = id
+        self.material_id = material_id
+        self.price = price
+        self.date = date

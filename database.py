31c003f0ff988f8c7df227d666e5eb2005fa0f94
Sobhan <sobhan.saf79@gmail.com>
2020-09-13 10:15:43 +0430

    two methods to add material and get id of a material is added

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ class DB:
         self.cur.executescript('''
         CREATE TABLE IF NOT EXISTS Materials (
             material_id INTEGER NOT NULL UNIQUE,
-            name VARCHAR(50),
+            name VARCHAR(50) NOT NULL,
 
             PRIMARY KEY(material_id AUTOINCREMENT)
         );
@@ -22,8 +22,14 @@ class DB:
             FOREIGN KEY(material_id) REFERENCES Materials(material_id),
             PRIMARY KEY(price_id AUTOINCREMENT)
         );
-        
 
         ''')
+    def get_material_id(self, name):
+        # gets the name of a material and returns its id in database
+        # returns none if not found in database
+        self.cur.execute('SELECT material_id FROM materials WHERE name=?', (name, ))
+        row = self.cur.fetchone()
+        return None if row is None else row[0]
 
-
+a = DB()
+a.get_material_id('test')

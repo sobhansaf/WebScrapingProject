7666a2b68c3f4d1af2ba827072fb02c5b493fdba
Sobhan <sobhan.saf79@gmail.com>
2020-09-13 23:06:54 +0430

    scrap.py script now has a function to scrap data and returns database object

--- scrap.py
+++ scrap.py
@@ -8,45 +8,46 @@ from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 from time import sleep
 
+def scrap(dbfilename, waiting_time, path_to_driver):
 
-url = 'https://www.tgju.org/'
+    url = 'https://www.tgju.org/'
 
-option = webdriver.ChromeOptions()
-option.add_argument('headless')
-driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=option)
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    driver = webdriver.Chrome(path_to_driver ,options=option)
 
-try:
-    driver.get(url)
-except:
-    print('can\'t connect to url.')
-    exit(-1)
+    try:
+        driver.get(url)
+    except:
+        print('can\'t connect to url.')
+        exit(-1)
 
-sleep(5)
-table_class_name = 'market-table'
+    sleep(waiting_time)
+    table_class_name = 'market-table'
 
+    html = driver.page_source
+    driver.close()
+    soup = BeautifulSoup(html, 'html.parser')
+    tables = soup.select(f'.{table_class_name}')
 
-html = driver.page_source
-driver.close()
-soup = BeautifulSoup(html, 'html.parser')
-tables = soup.select(f'.{table_class_name}')
+    db = database.DB(dbfilename)
 
-db = database.DB()
+    for table in tables:
+        items = table.select('tbody tr')
+        for item in items:
+            name = item.find('th').text
+            price = item.find('td').text
+        
+            price = price.replace('میلیون', '')  # some of prices has additional words
+            price = price.replace(',', '')
+            try:  # price should be numeric
+                price = int(price)
+            except:
+                continue
 
-file = open('test', 'w')
-for table in tables:
-    items = table.select('tbody tr')
-    for item in items:
-        name = item.find('th').text
-        price = item.find('td').text
-    
-        price = price.replace('میلیون', '')  # some of prices has additional words
-        price = price.replace(',', '')
-        try:  # price should be numeric
-            price = int(price)
-        except:
-            continue
-
-        db.add_material(name)
-        utctime = int((datetime.now() - datetime(1970, 1, 1)).total_seconds())
-        db.add_price(name, utctime, price)
+            db.add_material(name)
+            utctime = int((datetime.now() - datetime(1970, 1, 1)).total_seconds())
+            db.add_price(name, utctime, price)
+            
+    return db
 
